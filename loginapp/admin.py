a760225870a348f7a6d03bd3a6a91cd6cbcce7e2
Dinesh Wagh <86907745+dineshwagh1996@users.noreply.github.com>
2021-10-13 18:18:32 +0530

    Add files via upload

--- loginapp/admin.py
+++ loginapp/admin.py
@@ -1,15 +1,6 @@
 from django.contrib import admin
-from loginapp.models import Regester
-from loginapp.models import dipakdata
-from loginapp.models import dipdata
-from loginapp.models import saurabhdata
-from loginapp.models import maheshdata
-from loginapp.models import swatidata
+from loginapp.models import Regester,Prod
 
 
 admin.site.register(Regester)
-admin.site.register(dipakdata)
-admin.site.register(dipdata)
-admin.site.register(saurabhdata)
-admin.site.register(maheshdata)
-admin.site.register(swatidata)
+admin.site.register(Prod)
